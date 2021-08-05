87a7b4c97237aecc6281e6ebb5fb63660e94b95b
hendrasoewarno <hendrasoewarno@gmail.com>
2021-08-05 13:22:15 +0700

    Update logger.php

--- www/fail2ban/logger.php
+++ www/fail2ban/logger.php
@@ -15,14 +15,36 @@ CREATE TABLE IF NOT EXISTS `fail2ban` (
 */
 include "framework/library.php";
 
+function sendMessage($chat_id, $text) {
+  $botToken="<BOT_TOKEN_HERE>";
+  $api_url="https://api.telegram.org/bot".$botToken;
+  $params=[
+      'chat_id'=>$chat_id, 
+      'text'=> $text,
+  ];
+  $ch = curl_init($api_url . '/sendMessage');
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  logDebug(result);
+  curl_close($ch);
+}
+
 if (getClientIPAddress()=='127.0.0.1') {
 
 $sql = <<<EOD
 Insert Into fail2ban(actiontimestamp, sourceipaddr, rule, attempts, whois, log) Values (now(), :1, :2, :3, :4, :5);
 EOD;
 
-	createRow(openConnection(), $sql, array("sourceipaddr", "rule", 1, "whois", "log"));
-	//createRow(openConnection(), $sql, array(param("sourceipaddr"), param("rule"), param("attempts"), param("whois"), param("log")));
+	//createRow(openConnection(), $sql, array("sourceipaddr", "rule", 1, "whois", "log"));
+	//log to db
+	createRow(openConnection(), $sql, array(param("sourceipaddr"), param("rule"), param("attempts"), param("whois"), param("log")));
+	//send telegram
+	sendMessage(568577002,
+		"The IP " . param("sourceipaddr") . " has just been banned by Fail2Ban after " . param("attempts") . "  attempts against " .  param("rule") . "\n" . param("whois") . "\n" . param("log"));
 
 } else {
 	logDebug("call from outsider");
