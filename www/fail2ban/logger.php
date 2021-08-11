54c275dd3b399a63b4d50f2c1a41006796c56930
hendrasoewarno <hendrasoewarno@gmail.com>
2021-08-11 14:49:37 +0700

    Update logger.php

--- www/fail2ban/logger.php
+++ www/fail2ban/logger.php
@@ -3,17 +3,21 @@
 CREATE TABLE IF NOT EXISTS `fail2ban` (
   `actiontimestamp` datetime NOT NULL,
   `sourceipaddr` varchar(15) NOT NULL,
+  `inetnum` varchar(40),
+  `name` varchar(100),
+  `organization` varchar(100),
+  `country` varchar(2),
   `rule` varchar(30) NOT NULL,
   `attempts` int(11) NOT NULL,
-  `whois` text NOT NULL,
   `log` text NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `actiontimestamp` (`actiontimestamp`),
   KEY `sourceipaddr` (`sourceipaddr`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 */
 include "framework/library.php";
+include "phpWhois.org-master/whois.main.php";
 
 function sendMessage($chat_id, $text) {
   $botToken="<BOT_TOKEN_HERE>";
@@ -39,19 +43,37 @@ $sql = <<<EOD
 Insert Into fail2ban(actiontimestamp, sourceipaddr, rule, attempts, whois, log) Values (now(), :1, :2, :3, :4, :5);
 EOD;
 
-	//createRow(openConnection(), $sql, array("sourceipaddr", "rule", 1, "whois", "log"));
 	//log to db
-	createRow(openConnection(), $sql, array(param("sourceipaddr"), param("rule"), param("attempts"), param("whois"), param("log")));
-	//send telegram
-	//send telegram
-	sendMessage(568577002, "The IP " . param("sourceipaddr") . " has just been banned by Fail2Ban after " . param("attempts") . "  attempts against" . param("rule"));
-	sendMessage(568577002, substr(param("whois"),0,4096));
-	sendMessage(568577002, param("log"));
+	$conn=openConnection();
+	createRow($conn, $sql, array(param("sourceipaddr"), param("rule"), param("attempts"), param("log")));
+	$id = $conn->lastInsertId();
+	
+
+$sql = <<<EOD
+Update fail2ban Set inetnum=:1, name=:2, organization=:3, country=:4 Where id=:5;
+EOD;
+	
+	$whois = new Whois();
+	$result = $whois->Lookup(param("sourceipaddr"),false);
+	
+	if ($result['regrinfo']['registered'] == 'yes') {
+		updateRow($conn, $sql, array(
+			$result['regrinfo']['network']['inetnum'],
+			$result['regrinfo']['network']['name'],
+			$result['regrinfo']['network']['organization'],
+			$result['regrinfo']['network']['country'],
+			$id)
+		);
+		//send telegram
+		sendMessage(568577002, "The IP " . param("sourceipaddr") . "(" . $result['regrinfo']['network']['name'] . "[" . $result['regrinfo']['network']['country'] . "]) has just been banned by Fail2Ban after " . param("attempts") . "  attempts against " . param("rule") . "with logs " . param("log"));
+	} else {
+		sendMessage(568577002, "The IP " . param("sourceipaddr") . " has just been banned by Fail2Ban after " . param("attempts") . "  attempts against " . param("rule") . "with logs " . param("log"));
+	}
 } else {
 	logDebug("call from outsider");
 }
 
 //unit test
-//curl -X POST http://localhost/fail2ban/logger.php -d sourceipaddr="<ip>" -d rule="<name>" -d attempts=<failures> -d whois="`/usr/bin/whois -H <ip>`" -d log="`/bin/grep '\<<ip>\>' <logpath> | tail -n <failures>`"
-//curl -k -X POST https://localhost/fail2ban/logger.php -d sourceipaddr="<ip>" -d rule="<name>" -d attempts=<failures> -d whois="`/usr/bin/whois -H <ip>`" -d log="`/bin/grep '\<<ip>\>' <logpath> | tail -n <failures>`"
+//curl -X POST http://localhost/fail2ban/logger.php -d sourceipaddr="<ip>" -d rule="<name>" -d attempts=<failures> -d log="`/bin/grep '\<<ip>\>' <logpath> | tail -n <failures>`"
+//curl -k -X POST https://localhost/fail2ban/logger.php -d sourceipaddr="<ip>" -d rule="<name>" -d attempts=<failures> -d log="`/bin/grep '\<<ip>\>' <logpath> | tail -n <failures>`"
 ?>
